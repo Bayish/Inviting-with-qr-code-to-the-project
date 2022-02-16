50e2253c8857a4624b7b23c5eedf6a6620516e49
nkazbekov23 <nursultankazbekov23@gmail.com>
2022-02-16 14:59:26 +0600

    change get file url

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -22,9 +22,9 @@ const GalleryItem = ({name, setId, id,  user, removeHandler, fileName}) => {
     if (typeof fileName !== 'undefined') {
       const format = fileName.split('.');
       if(format[1] === 'mp4'){
-         video = 'https://sferakg.ru/file/' + fileName;
+         video = 'http://3.111.156.110/file/' + fileName;
       }else{
-        image ='https://sferakg.ru/file/' + fileName;
+        image ='http://3.111.156.110/file/' + fileName;
       }
     }
 
